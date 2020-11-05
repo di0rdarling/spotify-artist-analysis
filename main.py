e059edca69f14610493680bdf0b6b97aca023741
Abisola <h.fagbemi@yahoo.co.uk>
2020-11-05 16:29:54 +0000

    Added class methods to get top artist and top tracks.

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from spotify_client import SpotifyClient
 def main():
     artist_query = input('Enter an artist: \n')
     client = SpotifyClient()
-    client.get_artist_details(artist_query)
+    top_songs = client.get_artist_top_songs(artist_query, 5)
+
+    print('Here is ' + artist_query + '\'s top 5 songs:')
+    for index, track in enumerate(top_songs):
+        print(index, track[0])
 
 
 main()
--- spotify_client.py
+++ spotify_client.py
@@ -7,19 +7,19 @@ class SpotifyClient:
     def __init__(self):
         pass
 
-    def get_artist_details(self, query):
+    @classmethod
+    def execute_query(cls, query, query_type, limit):
 
-        def execute_query(self, query, queryType, limit):
+        scope = "user-library-read"
+        sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ['SPOTIPY_CLIENT_ID'],
+                                                                   client_secret=os.environ['SPOTIPY_CLIENT_SECRET']))
+        results = sp.search(q=query, limit=limit, type=query_type)
+        return results
 
-            scope = "user-library-read"
-            sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ['SPOTIPY_CLIENT_ID'],
-                                                                       client_secret=os.environ['SPOTIPY_CLIENT_SECRET']))
-            results = sp.search(q=query, limit=limit, type=queryType)
+    def get_top_artist(self, artist_query):
 
-            return results
-
-        # From the results find the top artist based on popularity.
-        artists_results = execute_query(self, query, 'artist', 5)
+        # From the results find the top artist based on the popularity rating.
+        artists_results = self.execute_query(artist_query, 'artist', 5)
         top_result = None
         for idx, artist in enumerate(artists_results['artists']['items']):
             if top_result is None:
@@ -30,7 +30,23 @@ class SpotifyClient:
                 if artist_popularity > top_result_popularity:
                     top_result = artist
 
+        return top_result
+
+    def get_artist_top_songs(self, artist_query, limit):
+
+        artist = self.get_top_artist(artist_query)
         # Fetch the artists tracks.
-        artist_tracks = execute_query(self, top_result['name'], 'track', 50)
-        for idx, track in enumerate(artist_tracks['tracks']['items']):
-            print(idx, track['name'], track['popularity'], )
+        tracks_details_full = self.execute_query(
+            artist['name'], 'track', 50)
+
+        tracks_details_canonical = []
+        for index, track in enumerate(tracks_details_full['tracks']['items']):
+            # Retrieve only the name and popularity rating from each track.
+            tracks_details_canonical.append(
+                [track['name'], track['popularity']])
+
+        # Sort the tracks by popularity
+        tracks_details_canonical.sort(key=lambda t: t[1], reverse=True)
+        top_songs = tracks_details_canonical[:limit]
+
+        return top_songs
