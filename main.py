801e04aaa47d32d19c23ca24f69db30822a8b0e6
Abisola <h.fagbemi@yahoo.co.uk>
2020-11-06 10:50:19 +0000

    Added method to fetch artists latest song.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from spotify_client import SpotifyClient
+import datetime
 
 
 def main():
     artist_query = input('Enter an artist: \n')
     client = SpotifyClient()
     top_songs = client.get_artist_top_songs(artist_query, 5)
-
     print('Here is ' + artist_query + '\'s top 5 songs:')
     for index, track in enumerate(top_songs):
-        print(index, track[0])
+        print(index+1, track['name'])
+
+    latest_song = client.get_artist_latest_song(artist_query)
+    print('\nHere is there latest song:')
+    print(latest_song['name'] + ' released on ' +
+          str(latest_song['release_date'].ctime()))
 
 
 main()
--- spotify_client.py
+++ spotify_client.py
@@ -1,6 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import os
+import datetime
 
 
 class SpotifyClient:
@@ -42,11 +43,39 @@ class SpotifyClient:
         tracks_details_canonical = []
         for index, track in enumerate(tracks_details_full['tracks']['items']):
             # Retrieve only the name and popularity rating from each track.
-            tracks_details_canonical.append(
-                [track['name'], track['popularity']])
+            tracks_details_canonical.append({'name':
+                                             track['name'], 'popularity': track['popularity']})
 
         # Sort the tracks by popularity
-        tracks_details_canonical.sort(key=lambda t: t[1], reverse=True)
+        tracks_details_canonical.sort(
+            key=lambda t: t['popularity'], reverse=True)
         top_songs = tracks_details_canonical[:limit]
 
         return top_songs
+
+    def get_artist_latest_song(self, artist_query):
+
+        artist = self.get_top_artist(artist_query)
+        # Fetch the artists tracks.
+        tracks_details_full = self.execute_query(
+            artist['name'], 'track', 50)
+
+        latest_track = None
+        for index, track in enumerate(tracks_details_full['tracks']['items']):
+            track_name = track['name']
+            # Convert the release date to a valid date object.
+            # e.g. datetime string 2020-10-31
+            release_date_string = track['album']['release_date']
+            release_date_arr = release_date_string.split('-')
+            release_datetime = datetime.datetime(
+                int(release_date_arr[0]), int(release_date_arr[1]), int(release_date_arr[2]))
+
+            if latest_track is None:
+                latest_track = {'name': track_name,
+                                'release_date': release_datetime}
+            else:
+                if release_datetime > latest_track['release_date']:
+                    latest_track = {'name': track_name,
+                                    'release_date': release_datetime}
+
+        return latest_track
