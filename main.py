802d5eaf3d7ef7f4998455491ca8dc7ccee4039a
Abisola <h.fagbemi@yahoo.co.uk>
2020-11-06 11:53:12 +0000

    Method to get artist genres and class docstrings.

--- main.py
+++ main.py
@@ -5,15 +5,21 @@ import datetime
 def main():
     artist_query = input('Enter an artist: \n')
     client = SpotifyClient()
-    top_songs = client.get_artist_top_songs(artist_query, 5)
-    print('Here is ' + artist_query + '\'s top 5 songs:')
+    top_songs = client.get_artist_top_songs(artist_query)
+    print('Here is ' + artist_query + '\'s top ' +
+          str(len(top_songs)) + ' songs:')
     for index, track in enumerate(top_songs):
         print(index+1, track['name'])
 
     latest_song = client.get_artist_latest_song(artist_query)
-    print('\nHere is there latest song:')
+    print('\nHere is their latest song:')
     print(latest_song['name'] + ' released on ' +
           str(latest_song['release_date'].ctime()))
+
+    print('\nHere are their related genres:')
+    artists_genres = client.get_artist_genres(artist_query)
+    for genre in artists_genres:
+        print(genre)
 
 
 main()
--- spotify_client.py
+++ spotify_client.py
@@ -5,19 +5,49 @@ import datetime
 
 
 class SpotifyClient:
+    """
+    A class to represent a Spotify Client
+    ...
+
+    Methods
+    -------
+    execute_query(query, query_type, limit=10):
+        Returns the results of a given query.
+    get_top_artist(artist_query:
+        Returns the top artists for a given artist query.
+    get_artist_top_songs(artist_query, limit=5):
+        Returns the top songs for a given artist query.
+    get_artist_latest_song(artist_query):
+        Returns the latest song for a given artist query.
+    get_artist_genres(artist_query):
+        Returns the list of related genres for a given artist query.
+    """
+
     def __init__(self):
         pass
 
     @classmethod
-    def execute_query(cls, query, query_type, limit):
+    def execute_query(cls, query, query_type, limit=10):
+        '''
+        Takes in a query, query type, and limit and returns the search results.
 
-        scope = "user-library-read"
+        Parameters
+            query - the search query
+            query_type - the type of item to return. One of 'artist', 'album', 'track', 'playlist', 'show', or 'episode'
+            limit  - the number of items to return (min = 1, default = 10, max = 50)
+        '''
         sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ['SPOTIPY_CLIENT_ID'],
                                                                    client_secret=os.environ['SPOTIPY_CLIENT_SECRET']))
         results = sp.search(q=query, limit=limit, type=query_type)
         return results
 
     def get_top_artist(self, artist_query):
+        '''
+        Takes in an artist query, returns the top artist found by popularity.
+
+        Parameters
+            artist_query - the artist search query
+        '''
 
         # From the results find the top artist based on the popularity rating.
         artists_results = self.execute_query(artist_query, 'artist', 5)
@@ -33,7 +63,14 @@ class SpotifyClient:
 
         return top_result
 
-    def get_artist_top_songs(self, artist_query, limit):
+    def get_artist_top_songs(self, artist_query, limit=5):
+        '''
+        Takes in an artist query and limit, returns the artists top songs by popularity.
+
+        Parameters
+            artist_query - the artist search query
+            limit  - the number of top songs to return (min = 1, default = 5, max = 10)
+        '''
 
         artist = self.get_top_artist(artist_query)
         # Fetch the artists tracks.
@@ -41,7 +78,7 @@ class SpotifyClient:
             artist['name'], 'track', 50)
 
         tracks_details_canonical = []
-        for index, track in enumerate(tracks_details_full['tracks']['items']):
+        for track in tracks_details_full['tracks']['items']:
             # Retrieve only the name and popularity rating from each track.
             tracks_details_canonical.append({'name':
                                              track['name'], 'popularity': track['popularity']})
@@ -54,6 +91,12 @@ class SpotifyClient:
         return top_songs
 
     def get_artist_latest_song(self, artist_query):
+        '''
+        Takes in an artist query, returns the artist latest song.
+
+        Parameters
+            artist_query - the artist search query
+        '''
 
         artist = self.get_top_artist(artist_query)
         # Fetch the artists tracks.
@@ -61,7 +104,7 @@ class SpotifyClient:
             artist['name'], 'track', 50)
 
         latest_track = None
-        for index, track in enumerate(tracks_details_full['tracks']['items']):
+        for track in tracks_details_full['tracks']['items']:
             track_name = track['name']
             # Convert the release date to a valid date object.
             # e.g. datetime string 2020-10-31
@@ -79,3 +122,23 @@ class SpotifyClient:
                                     'release_date': release_datetime}
 
         return latest_track
+
+    def get_artist_genres(self, artist_query):
+        '''
+        Takes in an artist query, returns the artists related genres.
+
+        Parameters
+            artist_query - the artist search query
+        '''
+        artist = self.get_top_artist(artist_query)
+        # Fetch the artists tracks.
+        artist_details = self.execute_query(
+            artist['name'], 'artist')
+
+        genres = []
+        for detail in artist_details['artists']['items']:
+            if len(detail['genres']) > 0:
+                for genre in detail['genres']:
+                    genres.append(genre)
+
+        return genres
