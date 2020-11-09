dd6b6d508f8c873e56b9f49c78fdbc8814d66bb4
Abisola <h.fagbemi@yahoo.co.uk>
2020-11-09 11:32:25 +0000

    Search query to first be validated.

--- run.py
+++ run.py
@@ -5,21 +5,25 @@ import datetime
 def main():
     artist_query = input('Enter an artist: \n')
     client = SpotifyClient()
-    top_songs = client.get_artist_top_songs(artist_query)
-    print('Here is ' + artist_query + '\'s top ' +
-          str(len(top_songs)) + ' songs:')
-    for index, track in enumerate(top_songs):
-        print(index+1, track['name'])
+    top_artist = client.get_top_artist(artist_query)
+    if top_artist is not None:
+        top_songs = client.get_artist_top_songs(top_artist['name'])
+        print('Here is ' + artist_query + '\'s top ' +
+              str(len(top_songs)) + ' songs:')
+        for index, track in enumerate(top_songs):
+            print(index+1, track['name'])
 
-    latest_song = client.get_artist_latest_song(artist_query)
-    print('\nHere is their latest song:')
-    print(latest_song['name'] + ' released on ' +
-          str(latest_song['release_date'].ctime()))
+        latest_song = client.get_artist_latest_song(top_artist['name'])
+        print('\nHere is their latest song:')
+        print(latest_song['name'] + ' released on ' +
+              str(latest_song['release_date'].ctime()))
 
-    print('\nHere are their related genres:')
-    artists_genres = client.get_artist_genres(artist_query)
-    for genre in artists_genres:
-        print(genre)
+        print('\nHere are their related genres:')
+        artists_genres = client.get_artist_genres(top_artist['name'])
+        for genre in artists_genres:
+            print(genre)
+    else:
+        print('No results were found. Please enter another artist or a more valid search query.')
 
 
 main()
--- spotify_client.py
+++ spotify_client.py
@@ -51,15 +51,17 @@ class SpotifyClient:
 
         # From the results find the top artist based on the popularity rating.
         artists_results = self.execute_query(artist_query, 'artist', 5)
+        results = artists_results['artists']['items']
         top_result = None
-        for idx, artist in enumerate(artists_results['artists']['items']):
-            if top_result is None:
-                top_result = artist
-            else:
-                top_result_popularity = top_result['popularity']
-                artist_popularity = artist['popularity']
-                if artist_popularity > top_result_popularity:
+        if len(results) != 0:
+            for idx, artist in enumerate(results):
+                if top_result is None:
                     top_result = artist
+                else:
+                    top_result_popularity = top_result['popularity']
+                    artist_popularity = artist['popularity']
+                    if artist_popularity > top_result_popularity:
+                        top_result = artist
 
         return top_result
 
@@ -72,21 +74,20 @@ class SpotifyClient:
             limit  - the number of top songs to return (min = 1, default = 5, max = 10)
         '''
 
-        artist = self.get_top_artist(artist_query)
+        top_songs = None
         # Fetch the artists tracks.
-        tracks_details_full = self.execute_query(
-            artist['name'], 'track', 50)
-
+        tracks_details_full = self.execute_query(artist_query, 'track', 50)
         tracks_details_canonical = []
-        for track in tracks_details_full['tracks']['items']:
-            # Retrieve only the name and popularity rating from each track.
-            tracks_details_canonical.append({'name':
-                                             track['name'], 'popularity': track['popularity']})
+        if len(tracks_details_full['tracks']['items']) > 0:
+            for track in tracks_details_full['tracks']['items']:
+                # Retrieve only the name and popularity rating from each track.
+                tracks_details_canonical.append({'name':
+                                                 track['name'], 'popularity': track['popularity']})
 
-        # Sort the tracks by popularity
-        tracks_details_canonical.sort(
-            key=lambda t: t['popularity'], reverse=True)
-        top_songs = tracks_details_canonical[:limit]
+                # Sort the tracks by popularity
+                tracks_details_canonical.sort(
+                    key=lambda t: t['popularity'], reverse=True)
+                top_songs = tracks_details_canonical[:limit]
 
         return top_songs
 
@@ -98,28 +99,28 @@ class SpotifyClient:
             artist_query - the artist search query
         '''
 
-        artist = self.get_top_artist(artist_query)
         # Fetch the artists tracks.
-        tracks_details_full = self.execute_query(
-            artist['name'], 'track', 50)
+        tracks_details_full = self.execute_query(artist_query, 'track', 50)
 
         latest_track = None
-        for track in tracks_details_full['tracks']['items']:
-            track_name = track['name']
-            # Convert the release date to a valid date object.
-            # e.g. datetime string 2020-10-31
-            release_date_string = track['album']['release_date']
-            release_date_arr = release_date_string.split('-')
-            release_datetime = datetime.datetime(
-                int(release_date_arr[0]), int(release_date_arr[1]), int(release_date_arr[2]))
+        if len(tracks_details_full['tracks']['items']) > 0:
+            for track in tracks_details_full['tracks']['items']:
+                track_name = track['name']
+                # Convert the release date to a valid date object.
+                # e.g. datetime string 2020-10-31
+                release_date_string = track['album']['release_date']
+                release_date_arr = release_date_string.split('-')
+                if len(release_date_arr) == 3:
+                    release_datetime = datetime.datetime(
+                        int(release_date_arr[0]), int(release_date_arr[1]), int(release_date_arr[2]))
 
-            if latest_track is None:
-                latest_track = {'name': track_name,
-                                'release_date': release_datetime}
-            else:
-                if release_datetime > latest_track['release_date']:
+                if latest_track is None:
                     latest_track = {'name': track_name,
                                     'release_date': release_datetime}
+                else:
+                    if release_datetime > latest_track['release_date']:
+                        latest_track = {'name': track_name,
+                                        'release_date': release_datetime}
 
         return latest_track
 
@@ -130,15 +131,14 @@ class SpotifyClient:
         Parameters
             artist_query - the artist search query
         '''
-        artist = self.get_top_artist(artist_query)
         # Fetch the artists tracks.
-        artist_details = self.execute_query(
-            artist['name'], 'artist')
+        artist_details = self.execute_query(artist_query, 'artist')
 
         genres = []
-        for detail in artist_details['artists']['items']:
-            if len(detail['genres']) > 0:
-                for genre in detail['genres']:
-                    genres.append(genre)
+        if len(artist_details['artists']['items']) > 0:
+            for detail in artist_details['artists']['items']:
+                if len(detail['genres']) > 0:
+                    for genre in detail['genres']:
+                        genres.append(genre)
 
         return genres
